dbf99c62dece5cbe4fde669f9cc53ecf5137b108
Anass EL <evolutionstudio3000@gmail.com>
2023-07-20 17:45:55 +0200

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,103 @@
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = 5000;
+
+// const products = []; // In-memory storage for products (Replace this with a database in a real application)
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Get all products
+// app.get('/products', (req, res) => {
+//   res.json(products);
+// });
+
+// // Add a new product
+// app.post('/products', (req, res) => {
+//   const newProduct = req.body;
+//   products.push(newProduct);
+//   res.json(newProduct);
+// });
+
+// // Update a product by ID
+// app.put('/products/:id', (req, res) => {
+//   const productId = req.params.id;
+//   const updatedProduct = req.body;
+
+//   const index = products.findIndex((product) => product._id === productId);
+//   if (index !== -1) {
+//     products[index] = { ...products[index], ...updatedProduct };
+//     res.json(products[index]);
+//   } else {
+//     res.status(404).json({ message: 'Product not found' });
+//   }
+// });
+
+// // Delete a product by ID
+// app.delete('/products/:id', (req, res) => {
+//   const productId = req.params.id;
+
+//   const index = products.findIndex((product) => product._id === productId);
+//   if (index !== -1) {
+//     products.splice(index, 1);
+//     res.json({ message: 'Product deleted successfully' });
+//   } else {
+//     res.status(404).json({ message: 'Product not found' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const app = express();
-const port = 5000;
-
-const products = []; // In-memory storage for products (Replace this with a database in a real application)
-
 app.use(cors());
 app.use(bodyParser.json());
 
-// Get all products
-app.get('/products', (req, res) => {
+let products = [];
+
+// CRUD operations for products
+app.post('/api/products', (req, res) => {
+  const product = req.body;
+  product.id = new Date().getTime().toString();
+  products.push(product);
+  res.status(201).json(product);
+});
+
+app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-// Add a new product
-app.post('/products', (req, res) => {
-  const newProduct = req.body;
-  products.push(newProduct);
-  res.json(newProduct);
+app.get('/api/products/:id', (req, res) => {
+  const productId = req.params.id;
+  const product = products.find((p) => p.id === productId);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: 'Product not found' });
+  }
 });
 
-// Update a product by ID
-app.put('/products/:id', (req, res) => {
+app.put('/api/products/:id', (req, res) => {
   const productId = req.params.id;
   const updatedProduct = req.body;
-
-  const index = products.findIndex((product) => product._id === productId);
-  if (index !== -1) {
-    products[index] = { ...products[index], ...updatedProduct };
-    res.json(products[index]);
-  } else {
-    res.status(404).json({ message: 'Product not found' });
-  }
+  products = products.map((p) => (p.id === productId ? { ...p, ...updatedProduct } : p));
+  res.json(updatedProduct);
 });
 
-// Delete a product by ID
-app.delete('/products/:id', (req, res) => {
+app.delete('/api/products/:id', (req, res) => {
   const productId = req.params.id;
-
-  const index = products.findIndex((product) => product._id === productId);
-  if (index !== -1) {
-    products.splice(index, 1);
-    res.json({ message: 'Product deleted successfully' });
-  } else {
-    res.status(404).json({ message: 'Product not found' });
-  }
+  products = products.filter((p) => p.id !== productId);
+  res.sendStatus(204);
 });
 
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -2,176 +2,181 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const BASE_API_URL = 'http://localhost:5000';
-
-function App() {
+const App = () => {
+  const [productName, setProductName] = useState('');
+  const [unitPrice, setUnitPrice] = useState('');
+  const [quantity, setQuantity] = useState('');
   const [products, setProducts] = useState([]);
-  const [newProductName, setNewProductName] = useState('');
-  const [newProductPrice, setNewProductPrice] = useState('');
-  const [newProductQuantity, setNewProductQuantity] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-  const [editingProductId, setEditingProductId] = useState('');
+  const [editingProductId, setEditingProductId] = useState(null);
+  const [originalProducts, setOriginalProducts] = useState([]); // New state for original products
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+ // State variables for editing inputs
+ const [editedName, setEditedName] = useState('');
+ const [editedUnitPrice, setEditedUnitPrice] = useState('');
+ const [editedQuantity, setEditedQuantity] = useState('');
 
-  const fetchProducts = async () => {
+ useEffect(() => {
+  fetchProducts();
+}, []);
+
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/api/products');
+    setProducts(response.data);
+    setOriginalProducts(response.data); // Store original products data
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+};
+
+  const addProduct = async () => {
     try {
-      const response = await axios.get(`${BASE_API_URL}/products`);
-      setProducts(response.data);
-    } catch (error) {
-      console.error('Error fetching products:', error);
-    }
-  };
-
-  const handleAddProduct = async () => {
-    try {
-      const newProduct = {
-        name: newProductName,
-        unitPrice: newProductPrice,
-        quantity: newProductQuantity,
-      };
-      await axios.post(`${BASE_API_URL}/products`, newProduct);
-      setNewProductName('');
-      setNewProductPrice('');
-      setNewProductQuantity('');
-      fetchProducts();
-      alert('Product is added successfully.');
+      const newProduct = { name: productName, unitPrice, quantity };
+      const response = await axios.post('http://localhost:5000/api/products', newProduct);
+      setProducts([...products, response.data]);
+      setProductName('');
+      setUnitPrice('');
+      setQuantity('');
     } catch (error) {
       console.error('Error adding product:', error);
     }
   };
 
-  const handleEditProduct = (productId, updatedProduct) => {
-    setProducts((prevProducts) => {
-      const updatedProducts = prevProducts.map((product) => {
-        if (product._id === productId) {
-          return updatedProduct;
-        }
-        return product;
-      });
-      return updatedProducts;
-    });
-  };
-
-  const handleDeleteProduct = async (productId) => {
+  const deleteProduct = async (productId) => {
     try {
-      await axios.delete(`${BASE_API_URL}/products/${productId}`);
-      fetchProducts();
+      await axios.delete(`http://localhost:5000/api/products/${productId}`);
+      setProducts(products.filter((p) => p.id !== productId));
     } catch (error) {
       console.error('Error deleting product:', error);
     }
   };
 
-  const handleSearch = async () => {
+  const updateProduct = async (productId) => {
     try {
-      if (!searchTerm) {
-        fetchProducts(); // If search term is empty, show all products
-      } else {
-        const response = await axios.get(`${BASE_API_URL}/products/search?name=${searchTerm}`);
-        setProducts(response.data);
-      }
+      const updatedProduct = {
+        name: editedName,
+        unitPrice: editedUnitPrice,
+        quantity: editedQuantity,
+      };
+      await axios.put(`http://localhost:5000/api/products/${productId}`, updatedProduct);
+      setProducts(
+        products.map((p) => (p.id === productId ? { ...p, ...updatedProduct } : p))
+      );
+      setEditingProductId(null);
     } catch (error) {
-      console.error('Error searching products:', error);
+      console.error('Error updating product:', error);
     }
   };
 
-  const handleKeyPress = (e) => {
+ 
+  const handleSearch = () => {
+    if (!searchTerm.trim()) {
+      // If the search term is empty, reset products to the original data
+      setProducts(originalProducts);
+    } else {
+      // Otherwise, filter products based on the search term
+      const filteredProducts = originalProducts.filter((p) =>
+        p.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setProducts(filteredProducts);
+    }
+  };
+
+  const handleShowAll = () => {
+    fetchProducts(); // Re-fetch all products
+    setSearchTerm(''); // Clear the search term
+  };
+
+  const handleSearchKeyPress = (e) => {
     if (e.key === 'Enter') {
       handleSearch();
     }
   };
 
-  const handleListAll = () => {
-    setSearchTerm(''); // Reset the search term
-    fetchProducts(); // Fetch all products
-  };
-
   return (
-    <div className="App">
-      <h1>Product CRUD App</h1>
-      <div className="form">
+    <div className="app">
+      <header className="header">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search Product"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyPress={handleSearchKeyPress} // Trigger search on Enter key press
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+      </header>
+      <div className="form-container">
+        <h2>Add a New Product</h2>
         <input
           type="text"
           placeholder="Product Name"
-          value={newProductName}
-          onChange={(e) => setNewProductName(e.target.value)}
+          value={productName}
+          onChange={(e) => setProductName(e.target.value)}
         />
         <input
-          type="text"
-          placeholder="Unit Price (EUR)"
-          value={newProductPrice}
-          onChange={(e) => setNewProductPrice(e.target.value)}
+          type="number" // Use type="number" for numeric input
+          placeholder="Unit Price in Euro"
+          value={unitPrice}
+          onChange={(e) => setUnitPrice(e.target.value)}
         />
         <input
-          type="number"
+          type="number" // Use type="number" for numeric input
           placeholder="Quantity"
-          value={newProductQuantity}
-          onChange={(e) => setNewProductQuantity(e.target.value)}
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
         />
-        <button onClick={handleAddProduct}>Add Product</button>
-        <input
-          type="text"
-          placeholder="Search by Name"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          onKeyPress={handleKeyPress} // Call handleKeyPress on key press
-        />
-        
-        <button onClick={handleSearch}>Search</button>
-        <button onClick={handleListAll}>List All</button> {/* Fixed the List All button */}
+        <button onClick={addProduct}>Add Product</button>
       </div>
-
+      <div className="search-container">
+        <button onClick={handleShowAll}>Show All</button>
+      </div>
       <div className="product-list">
-        {products.length === 0 ? (
-          <p>No products found.</p>
-        ) : (
-          products.map((product) => (
-            <div className="product" key={product._id}>
-              {editingProductId === product._id ? (
-                <>
-                  <input
-                    type="text"
-                    value={product.name}
-                    onChange={(e) => {
-                      const updatedProduct = { ...product, name: e.target.value };
-                      handleEditProduct(product._id, updatedProduct);
-                    }}
-                  />
-                  <input
-                    type="text"
-                    value={product.unitPrice}
-                    onChange={(e) => {
-                      const updatedProduct = { ...product, unitPrice: e.target.value };
-                      handleEditProduct(product._id, updatedProduct);
-                    }}
-                  />
-                  <input
-                    type="number"
-                    value={product.quantity}
-                    onChange={(e) => {
-                      const updatedProduct = { ...product, quantity: e.target.value };
-                      handleEditProduct(product._id, updatedProduct);
-                    }}
-                  />
-                  <button onClick={() => setEditingProductId('')}>Save</button>
-                </>
-              ) : (
-                <>
-                  <p>Name: {product.name}</p>
-                  <p>Unit Price: {product.unitPrice} EUR</p>
-                  <p>Quantity: {product.quantity}</p>
-                  <button onClick={() => setEditingProductId(product._id)}>Edit</button>
-                  <button onClick={() => handleDeleteProduct(product._id)}>Delete</button>
-                </>
-              )}
-            </div>
-          ))
-        )}
+      {products.length === 0 && <p>No products added yet</p>}
+        {products.map((product) => (
+          <div key={product.id} className="product-item">
+            {editingProductId === product.id ? (
+              <>
+                {/* Use type="number" for edited inputs */}
+                <input
+                  type="text"
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                />
+                <input
+                  type="number" // Use type="number" for numeric input
+                  value={editedUnitPrice}
+                  onChange={(e) => setEditedUnitPrice(e.target.value)}
+                />
+                <input
+                  type="number" // Use type="number" for numeric input
+                  value={editedQuantity}
+                  onChange={(e) => setEditedQuantity(e.target.value)}
+                />
+                <button onClick={() => updateProduct(product.id)}>Save</button>
+              </>
+            ) : (
+              <>
+                <p>Name: {product.name}</p>
+                <p>Unit Price: {product.unitPrice} Euro</p>
+                <p>Quantity: {product.quantity}</p>
+                <button onClick={() => {
+                  // Initialize editing inputs with the current product data
+                  setEditedName(product.name);
+                  setEditedUnitPrice(product.unitPrice);
+                  setEditedQuantity(product.quantity);
+                  setEditingProductId(product.id);
+                }}>Edit</button>
+              </>
+            )}
+            <button onClick={() => deleteProduct(product.id)}>Delete</button>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default App;
